cd4e851c25d0c66085a7e569f26c5df7f3b09e91
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-08-10 11:47:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+        alert("Hee hee that tickeles!");
+    }
+    input.addEventListener('click', clickAlert);
 }
+
+addingEventListener();
